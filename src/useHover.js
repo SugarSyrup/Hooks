072991ba96bc4e7b9916ac3d30dd7f897384a47c
JaYun <tlfvm04@naver.com>
2022-10-12 21:56:21 +0900

    useHover

---
+++ src/useHover.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+
+const useHover = (onHover) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("mouseenter", onHover);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("mouseenter", onHover);
+      }
+    };
+  }, []);
+
+  return typeof onHover === "function" ? element : undefined;
+};
+
+export default useHover;
